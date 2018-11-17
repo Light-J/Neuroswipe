0df2be7fe225766fbdeae769db1743e916626cce
c1726361 <lightjp@cardiff.ac.uk>
2018-11-17 13:36:27 +0000

    Added thymeleaf to serve a template, also added an api package with rest controller base and a controller base

--- src/main/java/com/nsa/cubric/application/controllers/RegistrationForm.java
+++ src/main/java/com/nsa/cubric/application/controllers/RegistrationForm.java
@@ -1,4 +1,20 @@
 package com.nsa.cubric.application.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/registration")
 public class RegistrationForm {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RegistrationForm.class);
+
+    @RequestMapping(value = "/")
+    public String register(){
+        LOG.debug("Handling GET request to /registration/");
+        return "register";
+    }
 }
--- src/main/java/com/nsa/cubric/application/controllers/api/RegistrationFormHandling.java
+++ src/main/java/com/nsa/cubric/application/controllers/api/RegistrationFormHandling.java
@@ -1,4 +1,14 @@
 package com.nsa.cubric.application.controllers.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/registration")
 public class RegistrationFormHandling {
+
+    @RequestMapping(value = "/test")
+    public String returnTest(){
+        return "This is my test";
+    }
 }
