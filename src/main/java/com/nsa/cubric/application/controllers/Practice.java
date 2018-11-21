7b986a50b3689ec4ecaa46a3e42d86fe7d511ecd
Calum Mcalinden <mcalindencm1@cardiff.ac.uk>
2018-11-21 22:37:30 +0000

    Adds practice controller

--- src/main/java/com/nsa/cubric/application/controllers/Practice.java
+++ src/main/java/com/nsa/cubric/application/controllers/Practice.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
-@RequestMapping(value = "/tutorial")
-public class Tutorial {
-    private static final Logger LOG = LoggerFactory.getLogger(Tutorial.class);
+@RequestMapping(value = "/practice")
+public class Practice {
+    private static final Logger LOG = LoggerFactory.getLogger(Practice.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showTutorialOverview(WebRequest webRequest, Model model){
-        LOG.debug("Handling GET request to /tutorial/");
-        return "tutorial_overview";
+        LOG.debug("Handling GET request to /practice/");
+        return "practice";
     }
 }
