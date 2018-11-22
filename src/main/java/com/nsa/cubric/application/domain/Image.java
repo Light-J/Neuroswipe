77c2ad7b507e0996e9555d9643aa88b0131e4261
Calum Mcalinden <mcalindencm1@cardiff.ac.uk>
2018-11-22 00:39:06 +0000

    Add getters to Image class

--- src/main/java/com/nsa/cubric/application/domain/Image.java
+++ src/main/java/com/nsa/cubric/application/domain/Image.java
@@ -1,11 +1,15 @@
 package com.nsa.cubric.application.domain;
 
 public class Image {
-    private Long id;
+
+    private int id;
     private String filename;
 
-    public Image(Long id, String filename) {
+    public Image(int id, String filename) {
         this.id = id;
         this.filename = filename;
     }
+
+    public String getFilename() {return filename;}
+    public int getId() {return id;}
 }
