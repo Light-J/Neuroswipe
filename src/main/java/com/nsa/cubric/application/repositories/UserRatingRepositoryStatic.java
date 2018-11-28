34498f8fb5a8574b5b7f3bd17d41affe3b7e1c22
Alexander Sibanda <SibandaAK@cardiff.ac.uk>
2018-11-28 18:38:46 +0000

    Ratings and user responses repositories and static version

---
+++ src/main/java/com/nsa/cubric/application/repositories/UserRatingRepositoryStatic.java
@@ -0,0 +1,13 @@
+package com.nsa.cubric.application.repositories;
+
+import com.nsa.cubric.application.domain.UserRating;
+
+import java.util.List;
+
+public interface UserRatingRepositoryStatic {
+	public void storeUserRatings(UserRating rating);
+
+	public List<UserRating> getAll();
+
+	public List<UserRating> getUserRatings(String userProfileId);
+}
