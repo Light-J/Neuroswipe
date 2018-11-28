60872191ea53cbb8cb0f7c647b53ae7af12a7dce
Calum Mcalinden <mcalindencm1@cardiff.ac.uk>
2018-11-28 18:17:56 +0000

    Adds controller for Admin interface

--- src/main/java/com/nsa/cubric/application/controllers/Admin.java
+++ src/main/java/com/nsa/cubric/application/controllers/Admin.java
@@ -1,4 +1,21 @@
 package com.nsa.cubric.application.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+@RequestMapping(value = "/admin")
 public class Admin {
+    private static final Logger LOG = LoggerFactory.getLogger(Practice.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showAdminPage(WebRequest webRequest, Model model){
+        LOG.debug("Handling GET request to /admin/");
+        return "admin";
+    }
 }
