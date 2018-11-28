1156678e4f28d42e937160e4e98dd7c5852327a7
Alexander Sibanda <SibandaAK@cardiff.ac.uk>
2018-11-28 18:36:04 +0000

    Ratings and response domains

---
+++ src/main/java/com/nsa/cubric/application/domain/UserResponse.java
@@ -0,0 +1,53 @@
+package com.nsa.cubric.application.domain;
+
+public class UserResponse {
+	private Long id;
+
+	private Integer userProfileId;
+
+	private Integer imageId;
+
+	private Boolean response;
+
+	public UserResponse() {
+	}
+
+	public UserResponse(Long id, Integer userProfileId, Integer imageId, Boolean response) {
+		this.id = id;
+		this.userProfileId = userProfileId;
+		this.imageId = imageId;
+		this.response = response;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getUserProfileId() {
+		return userProfileId;
+	}
+
+	public void setUserProfileId(Integer userProfileId) {
+		this.userProfileId = userProfileId;
+	}
+
+	public Integer getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(Integer imageId) {
+		this.imageId = imageId;
+	}
+
+	public Boolean getResponse() {
+		return response;
+	}
+
+	public void setResponse(Boolean response) {
+		this.response = response;
+	}
+}
