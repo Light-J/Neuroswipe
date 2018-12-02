7f2234a9b9e3b71dfd77afab80fc482247a5ccb3
c1726361 <lightjp@cardiff.ac.uk>
2018-12-02 10:32:41 +0000

    Added routes to admin rest controller

--- src/main/java/com/nsa/cubric/application/controllers/API/AdminAPI.java
+++ src/main/java/com/nsa/cubric/application/controllers/API/AdminAPI.java
@@ -4,11 +4,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("admin")
 public class AdminAPI {
 
     @PostMapping(value = "/removeUser")
     public String removeUser(@RequestParam Integer userId) {
+
+        return "success";
+    }
+
+    @PostMapping(value = "/removeUserResponses")
+    public String removeUserResponses(@RequestParam Integer userId){
 
         return "success";
     }
