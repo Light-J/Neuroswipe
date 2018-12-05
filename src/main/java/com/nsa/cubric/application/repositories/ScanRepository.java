d3fed3217764d8e7f2aedf1184606b708a164c81
Calum Mcalinden <calum@mcalinden.me.uk>
2018-12-05 19:36:47 +0000

    Changes table name

--- src/main/java/com/nsa/cubric/application/repositories/ScanRepository.java
+++ src/main/java/com/nsa/cubric/application/repositories/ScanRepository.java
@@ -12,13 +12,13 @@ import java.util.List;
 @Repository
 public class ScanRepository implements ScanRepositoryStatic {
     private JdbcTemplate jdbcTemplate;
-    private RowMapper<Scan> imageMapper;
+    private RowMapper<Scan> scanMapper;
 
     @Autowired
     public ScanRepository(JdbcTemplate aTemplate) {
         jdbcTemplate = aTemplate;
 
-        imageMapper = (rs, i) -> new Scan(
+        scanMapper = (rs, i) -> new Scan(
                 rs.getInt("id"),
                 rs.getString("path1"),
                 rs.getString("path2"),
@@ -31,8 +31,8 @@ public class ScanRepository implements ScanRepositoryStatic {
     public Scan findById(Long id){
         try{
             return jdbcTemplate.queryForObject(
-                    "select id, path1, path2, path3, known_good from images WHERE id = ?",
-                    new Object[]{id},imageMapper);
+                    "select id, path1, path2, path3, known_good from scans WHERE id = ?",
+                    new Object[]{id},scanMapper);
 
         }catch (EmptyResultDataAccessException e){
             return null;
@@ -43,22 +43,22 @@ public class ScanRepository implements ScanRepositoryStatic {
     @Override
     public void insert(Scan scan){
         jdbcTemplate.update(
-                "INSERT into images (path1, path2, path3, known_good) values (?, ?, ?, ?)",
+                "INSERT into scans (path1, path2, path3, known_good) values (?, ?, ?, ?)",
                 scan.getPath1(), scan.getPath2(), scan.getPath3(), scan.getKnownGood());
     }
 
     @Override
     public void updateKnownGood(Long id, Boolean knownGood) {
         jdbcTemplate.update(
-                "UPDATE images SET known_good = ? WHERE id = ?",
+                "UPDATE scans SET known_good = ? WHERE id = ?",
                 knownGood, id);
     }
 
     @Override
     public List<Scan> getAll(){
         return jdbcTemplate.query(
-                "SELECT id, path1, path2, path3, known_good FROM images",
-                new Object[]{},imageMapper
+                "SELECT id, path1, path2, path3, known_good FROM scans",
+                new Object[]{},scanMapper
         );
     }
 }
