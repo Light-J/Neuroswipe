f4c9599b1b25a4bb072e4b06aac87d773f392b48
c1726361 <lightjp@cardiff.ac.uk>
2018-12-05 19:29:00 +0000

    Changed to user email for removal

--- src/main/java/com/nsa/cubric/application/controllers/API/AdminAPI.java
+++ src/main/java/com/nsa/cubric/application/controllers/API/AdminAPI.java
@@ -1,5 +1,7 @@
 package com.nsa.cubric.application.controllers.API;
 
+import com.nsa.cubric.application.domain.Account;
+import com.nsa.cubric.application.services.AccountServiceStatic;
 import com.nsa.cubric.application.services.AdminServicesStatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +12,12 @@ public class AdminAPI {
     @Autowired
     AdminServicesStatic adminServices;
 
+    @Autowired
+    AccountServiceStatic accountService;
+
     @PostMapping(value = "/removeUser")
-    public Boolean removeUser(@RequestParam(value = "user_to_remove") String userId) {
+    public Boolean removeUser(@RequestParam(value = "user_to_remove") String userEmail) {
+        String userId = accountService.findByEmail(userEmail).getId().toString();
         return adminServices.removeUser(Integer.parseInt(userId));
     }
 
