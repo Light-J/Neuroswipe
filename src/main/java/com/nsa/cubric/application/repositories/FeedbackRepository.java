897dff5a404dc3660af47c4f763d10b13c469d24
Calum Mcalinden <calum@mcalinden.me.uk>
2018-12-10 14:52:26 +0000

    Adds feedback repository and implementation

---
+++ src/main/java/com/nsa/cubric/application/repositories/FeedbackRepository.java
@@ -0,0 +1,45 @@
+package com.nsa.cubric.application.repositories;
+
+import com.nsa.cubric.application.controllers.AccountDTO;
+import com.nsa.cubric.application.controllers.ProfileDTO;
+import com.nsa.cubric.application.domain.Account;
+import com.nsa.cubric.application.domain.Feedback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class FeedbackRepository implements FeedbackRepositoryStatic {
+    private JdbcTemplate jdbcTemplate;
+    private RowMapper<Feedback> feedbackMapper;
+
+    @Autowired
+    public FeedbackRepository(JdbcTemplate aTemplate) {
+        jdbcTemplate = aTemplate;
+
+        feedbackMapper = (rs, i) -> new Feedback(
+                rs.getLong("id"),
+                rs.getLong("userprofileid"),
+                rs.getString("feedback")
+        );
+    }
+
+    @Override
+    public void insertNewFeedback(Feedback feedback){
+        jdbcTemplate.update(
+                "INSERT into userfeedback (userprofileid, feedback) values (?,?)",
+                feedback.getUserProfileId(), feedback.getFeedback());
+    }
+
+    @Override
+    public List<Feedback> getAll(){
+        return jdbcTemplate.query(
+                "SELECT id, userprofileid, feedback FROM userfeedback",
+                new Object[]{},feedbackMapper
+        );
+    }
+}
