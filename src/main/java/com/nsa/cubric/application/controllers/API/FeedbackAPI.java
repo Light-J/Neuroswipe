f9626bb68ea673357504798de6778897584b760b
Calum Mcalinden <calum@mcalinden.me.uk>
2018-12-10 15:05:39 +0000

    Adds feedback API controller

--- src/main/java/com/nsa/cubric/application/controllers/API/FeedbackAPI.java
+++ src/main/java/com/nsa/cubric/application/controllers/API/FeedbackAPI.java
@@ -1,45 +1,55 @@
 package com.nsa.cubric.application.controllers.API;
 
-import com.nsa.cubric.application.controllers.RegistrationAccount;
 import com.nsa.cubric.application.domain.Account;
+import com.nsa.cubric.application.domain.Feedback;
 import com.nsa.cubric.application.services.AccountServiceStatic;
-import com.nsa.cubric.application.services.AdminServicesStatic;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.nsa.cubric.application.services.FeedbackServiceStatic;
+import com.nsa.cubric.application.services.LoggedUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static java.lang.Math.toIntExact;
+import java.util.List;
 
-@RestController("/admin/utilities")
-public class AdminAPI {
+@RestController("/feedback")
+public class FeedbackAPI {
 
-
-    private static final Logger LOG = LoggerFactory.getLogger(RegistrationAccount.class);
+    private FeedbackServiceStatic feedbackService;
 
     @Autowired
-    AdminServicesStatic adminServices;
+    public FeedbackAPI(FeedbackServiceStatic aRepo) {
+        feedbackService = aRepo;
+    }
+
+    @Autowired
+    LoggedUserService loggedUserService;
 
     @Autowired
     AccountServiceStatic accountService;
 
-    @PostMapping(value = "/removeUser")
-    public Boolean removeUser(@RequestParam(value = "user_to_remove") String userEmail) {
-        Long userId;
-        try{
-            userId = accountService.findByEmail(userEmail).getId();
-        } catch (NullPointerException e){
-            return false;
-        }
-        return adminServices.removeUser(userId);
+
+    /**
+     * This method is accepts posted JSON containing feedback information. It responds to POST
+     * requests to /feedback.
+     * @return Boolean true indicating success.
+     */
+    @PostMapping(value = "/")
+    public Boolean addFeedback(@RequestParam(value = "feedback") String feedbackText) {
+        Account loggedInUser = accountService.findByEmail(loggedUserService.getUsername());
+        Feedback feedback = new Feedback(null, loggedInUser.getId(), feedbackText);
+        feedbackService.insertNewFeedback(feedback);
+        return true;
     }
 
-    @PostMapping(value = "/removeUserResponses")
-    public Long removeUserResponses(@RequestParam(value = "user_to_remove_responses") String userEmail){
-        try{
-            return adminServices.removeUserResponses(accountService.findByEmail(userEmail).getId());
-        } catch (NullPointerException e){
-            return 0L;
-        }
+    /**
+     * This method is used to return the JSON for all the feedback entries. It responds to GET
+     * requests to /feedback.
+     * @return ResponseEntity object containing JSON.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity getFeedback() {
+        List<Feedback> feedback = feedbackService.getAll();
+        return new ResponseEntity<>(feedback, null, HttpStatus.OK);
     }
 }
