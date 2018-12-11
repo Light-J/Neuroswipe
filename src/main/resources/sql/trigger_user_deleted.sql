8658e31815a528adc9c4f38df4aa9b93e0a54f37
c1726361 <lightjp@cardiff.ac.uk>
2018-12-11 18:10:00 +0000

    Updated user deletion trigger

--- src/main/resources/sql/trigger_user_deleted.sql
+++ src/main/resources/sql/trigger_user_deleted.sql
@@ -1,14 +1,13 @@
 -- Trigger on DELETE
 -- When a user account is removed due to GDPR we need to set all responses and raitings to null
 DELIMITER //
-DROP TRIGGER IF EXISTS brainschema.TRIGGER_user_removed//
+DROP TRIGGER IF EXISTS brainschema.TRIGGER_user_deleted//
 USE brainschema//
 CREATE DEFINER = CURRENT_USER 
-TRIGGER brainschema.TRIGGER_user_removed 
+TRIGGER brainschema.TRIGGER_user_deleted 
 BEFORE DELETE ON useraccounts FOR EACH ROW
 BEGIN
 
-	UPDATE userresponses SET userprofileid = null WHERE userprofileid = (old.id);
     UPDATE userratings SET userprofileid = null WHERE userprofileid = (old.id);
 	DELETE FROM userprofiles WHERE id = old.id;
     
