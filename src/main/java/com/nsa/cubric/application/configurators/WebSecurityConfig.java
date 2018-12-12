f889a7aad4cebe699a37a98581a77dbd429f9f1f
Calum Mcalinden <calum@mcalinden.me.uk>
2018-12-12 15:04:03 +0000

    Redirect to homepage with URL fragment instead of login page

--- src/main/java/com/nsa/cubric/application/configurators/WebSecurityConfig.java
+++ src/main/java/com/nsa/cubric/application/configurators/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
             .formLogin()
+                .loginPage("/#login")
                 .successHandler(successHandler)
                 .loginProcessingUrl("/login")
                 .usernameParameter("username")
