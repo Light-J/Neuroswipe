e2deeff6ccda5511d5369cd4c11e0ca4de2f6866
Beast Jack <lightjp@cardiff.ac.uk>
2018-12-13 22:23:39 +0000

    Updated sql trigger to include removal of feedback

--- src/main/resources/sql/trigger_user_deleted.sql
+++ src/main/resources/sql/trigger_user_deleted.sql
@@ -7,8 +7,12 @@ CREATE DEFINER = CURRENT_USER
 TRIGGER brainschema.TRIGGER_user_deleted 
 BEFORE DELETE ON useraccounts FOR EACH ROW
 BEGIN
-
-    UPDATE userratings SET userprofileid = null WHERE userprofileid = (old.id);
+	DECLARE user_profile_id integer;
+    
+    SET user_profile_id := (SELECT userprofileid FROM userprofiles WHERE useraccountid = old.id);
+    
+    UPDATE userratings SET userprofileid = null WHERE userprofileid = user_profile_id;
+    DELETE FROM userfeedback WHERE userprofileid = user_profile_id;
 	DELETE FROM userprofiles WHERE id = old.id;
     
 END//
