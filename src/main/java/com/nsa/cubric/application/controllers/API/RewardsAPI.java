c2c7de50aecb0a90fdb93386eb978ed7088d3acc
jack <lightjp@cardiff.ac.uk>
2019-02-27 09:34:57 +0000

    Added api controller to get rewards as JSON

---
+++ src/main/java/com/nsa/cubric/application/controllers/API/RewardsAPI.java
@@ -0,0 +1,79 @@
+package com.nsa.cubric.application.controllers.API;
+
+import com.nsa.cubric.application.controllers.RegistrationController;
+import com.nsa.cubric.application.domain.Account;
+import com.nsa.cubric.application.domain.Scan;
+import com.nsa.cubric.application.services.AccountService;
+import com.nsa.cubric.application.services.AdminServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController()
+@RequestMapping("api/rewards")
+public class RewardsAPI {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(RegistrationController.class);
+
+    public RewardsAPI(){}
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity getUserRewards() {
+        Map<String, Integer> rewards_achieved = new HashMap<>();
+
+        //TODO update map with actual user rewards
+        rewards_achieved.put("Reward1", 1);
+        rewards_achieved.put("Reward2", 1);
+        rewards_achieved.put("Reward3", 0);
+        rewards_achieved.put("Reward4", 1);
+
+        return new ResponseEntity<>(rewards_achieved, null, HttpStatus.OK);
+    }
+
+
+
+    /*
+
+    @PostMapping(value = "/removeUser")
+    public Boolean removeUser(@RequestParam(value = "userToRemove") String userEmail) {
+        Long userId;
+        try{
+            userId = accountService.getAccountByEmail(userEmail).getId();
+        } catch (NullPointerException e){
+            return false;
+        }
+        return adminServices.removeUser(userId);
+    }
+
+    @PostMapping(value = "/removeUserResponses")
+    public Integer removeUserResponses(
+            @RequestParam(value = "userToRemoveResponses") String userEmail){
+        try{
+            return adminServices.removeUserResponses(accountService.getAccountByEmail(userEmail).getId());
+        } catch (NullPointerException e){
+            return 0;
+        }
+    }
+
+    @GetMapping(value = "/searchUsers")
+    public List<Account> searchUsers(
+            @RequestParam(value = "searchTerm") String searchTerm,
+            @RequestParam(value = "page") int page) {
+        return accountService.searchUsers(searchTerm, page);
+    }
+
+    @PostMapping(value = "/{userId}/role")
+    public boolean updateUserRole(
+            @PathVariable(value = "userId") Long userId,
+            @RequestBody() String role){
+
+        return adminServices.updateUserRole(userId, role.replace("role=", ""));
+    }
+*/
+}
