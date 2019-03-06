4e224e73fe8967e023c65846f5270f1134002504
jack <lightjp@cardiff.ac.uk>
2019-03-06 14:00:40 +0000

    Update scans domain to include bad reason

--- src/main/java/com/nsa/cubric/application/domain/Scan.java
+++ src/main/java/com/nsa/cubric/application/domain/Scan.java
@@ -7,13 +7,15 @@ public class Scan {
     private String sideImage;
     private String frontImage;
     private Boolean knownGood;
+    private String badReason;
 
-    public Scan(int id, String topImage, String sideImage, String frontImage, Boolean knownGood) {
+    public Scan(int id, String topImage, String sideImage, String frontImage, Boolean knownGood, String badReason) {
         this.id = id;
         this.topImage = topImage;
         this.sideImage = sideImage;
         this.frontImage = frontImage;
         this.knownGood = knownGood;
+        this.badReason = badReason;
     }
 
     public String getTopImage() {return topImage;}
@@ -21,10 +23,12 @@ public class Scan {
     public String getFrontImage() {return frontImage;}
     public int getId() {return id;}
     public Boolean getKnownGood() {return knownGood;}
+    public String getBadReason() { return badReason; }
 
     public void setTopImage(String topImage) { this.topImage = topImage;}
     public void setSideImage(String path1) { this.sideImage = sideImage;}
     public void setFrontImage(String path1) { this.frontImage = frontImage;}
     public void setId(int id) { this.id = id; }
     public void setKnownGood(Boolean knownGood) {this.knownGood = knownGood;}
+    public void setBadReason(String badReason) { this.badReason = badReason; }
 }
