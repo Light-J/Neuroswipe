e74a8e34e8813d50cca1116deb73089c0cd75dc7
jack <lightjp@cardiff.ac.uk>
2019-03-27 15:37:31 +0000

    Fixed lost option on users table with the added templating.

--- src/main/java/com/nsa/cubric/application/controllers/API/AccountAPI.java
+++ src/main/java/com/nsa/cubric/application/controllers/API/AccountAPI.java
@@ -2,17 +2,19 @@ package com.nsa.cubric.application.controllers.API;
 
 
 import com.nsa.cubric.application.services.AccountService;
-import com.nsa.cubric.application.services.registrationUtils.EmailValidator;
-import com.nsa.cubric.application.services.registrationUtils.ValidEmail;
-import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/account")
