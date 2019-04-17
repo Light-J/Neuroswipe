abae4711acfcefd0ec633c2018b4dccfd84268ee
c1724556 <downesj2@cardiff.ac.uk>
2019-04-17 12:35:17 +0100

    feedback form processes

--- src/main/java/com/nsa/cubric/application/controllers/FeedbackController.java
+++ src/main/java/com/nsa/cubric/application/controllers/FeedbackController.java
@@ -1,24 +1,44 @@
 package com.nsa.cubric.application.controllers;
 
+import com.nsa.cubric.application.domain.Feedback;
+import com.nsa.cubric.application.domain.FeedbackForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "/userfeedback")
 public class FeedbackController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String showFeedbackForm(){
+    public String showFeedbackForm(Model model){
+
+        FeedbackForm feedbackForm = new FeedbackForm();
+        model.addAttribute("FeedbackForm", feedbackForm);
+
         return "feedback_form";
     }
 
     @RequestMapping(value = "/submitted", method = RequestMethod.GET)
     public String showFeedbackSubmittedPage(){
+        return "feedback_submitted";
+    }
+
+    @PostMapping(value = "/submitfeedback")
+    public String submitForm(@Valid FeedbackForm feedForm, BindingResult bindingResult, Model model){
+
+        System.out.println(feedForm.getAccessibility());
+        System.out.println(feedForm.getInformation1());
+
         return "feedback_submitted";
     }
 }
---
+++ src/main/java/com/nsa/cubric/application/domain/FeedbackForm.java
@@ -0,0 +1,68 @@
+package com.nsa.cubric.application.domain;
+
+public class FeedbackForm {
+
+    private String information1;
+    private String information2;
+    private String training;
+    private String sorting;
+    private String reward;
+    private String easeOfUse;
+    private String accessibility;
+
+    public String getInformation1() {
+        return information1;
+    }
+
+    public void setInformation1(String information1) {
+        this.information1 = information1;
+    }
+
+    public String getInformation2() {
+        return information2;
+    }
+
+    public void setInformation2(String information2) {
+        this.information2 = information2;
+    }
+
+    public String getTraining() {
+        return training;
+    }
+
+    public void setTraining(String training) {
+        this.training = training;
+    }
+
+    public String getSorting() {
+        return sorting;
+    }
+
+    public void setSorting(String sorting) {
+        this.sorting = sorting;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+
+    public void setReward(String reward) {
+        this.reward = reward;
+    }
+
+    public String getEaseOfUse() {
+        return easeOfUse;
+    }
+
+    public void setEaseOfUse(String easeOfUse) {
+        this.easeOfUse = easeOfUse;
+    }
+
+    public String getAccessibility() {
+        return accessibility;
+    }
+
+    public void setAccessibility(String accessibility) {
+        this.accessibility = accessibility;
+    }
+}
