885ef80e33bf511ba22b3e6ab145e7dba9bdcf1d
jack <lightjp@cardiff.ac.uk>
2019-04-24 10:39:42 +0100

    Modified insert_images.py to only insert valid and new images

--- insert_images.py
+++ insert_images.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import random
 import os
+import getpass
 
-host = "localhost"#input("Please enter your database url (ie localhost): ")
-user = "root"#input("Please enter your database username: ")
-password = "comsc"#input("Please enter your database password: ")
+host = raw_input("Please enter your database url: ")
+user = raw_input("Please enter your database user username: ")
+password = getpass.getpass(prompt="Enter your datasource password: ")#str(input("Please enter your database password: "))
 database = "brainschema"
 
 #Credentials for database
@@ -27,8 +28,15 @@ def get_all_valid_scans():
        if scan_name in files_side:
            if scan_name in files_front:
                valid.insert(0, scan_name)
-               
     return valid
+
+def insert_scans(scans):
+    sql_scans = "INSERT INTO brainschema.scan (top_image, side_image, front_image) VALUES (%s, %s, %s)"
+    for scan in scans:
+        val_scan = ("top/" + scan, "side/"+scan, "front/"+scan)
+        mycursor.execute(sql_scans, val_scan)
+        mydb.commit()
+    print(str(len(scans)) + " scans have been entered succesfully");
 
 def check_for_new_scans(valid):
     existing_scans = []
@@ -37,35 +45,16 @@ def check_for_new_scans(valid):
     mycursor.execute(sql_scans)
     
     for row in mycursor.fetchall():
-        existing_scans.insert(0, row[0].strip("front/"))
+        existing_scans.insert(0, row[0].strip("top/"))
 
     for scan_name in valid:
         if scan_name not in existing_scans:
-            #print(scan_name)
             scans_to_be_added.insert(0, scan_name)
     
     for x in scans_to_be_added:
         print(x)
+    insert_scans(scans_to_be_added)
     
     
 check_for_new_scans(get_all_valid_scans())
-
-
-
-#Warning, images need to be in top, middle and side folders and must have the same names and same amount
-#I used the CUBRIC .zip images which work well for this
-def insert_images():
-   sql_scans = "INSERT INTO brainschema.scan (top_image, side_image, front_image) VALUES (%s, %s, %s)"
-   path = '.' 
-   files_front = os.listdir(path+"/front")
-   files_side = os.listdir(path+"/side")
-   files_top = os.listdir(path+"/top")
-   for i in range(0, len(files_front)):
-      val_scans = ("front/" + files_front[i], "side/"+files_side[i], "top/"+files_top[i])
-      mycursor.execute(sql_scans, val_scans)
-      mydb.commit()
-   input("Image entry succesful")
-      
-#insert_images()
-    
 
