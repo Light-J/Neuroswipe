4ab57e8094a23ba991d7161f9614b4b27a0d517f
jack <lightjp@cardiff.ac.uk>
2019-06-27 11:09:21 +0100

    Remove old code from feedback api

--- src/main/java/com/nsa/cubric/application/controllers/API/FeedbackAPI.java
+++ src/main/java/com/nsa/cubric/application/controllers/API/FeedbackAPI.java
@@ -28,39 +28,6 @@ public class FeedbackAPI {
         this.feedbackService = feedbackService;
     }
 
-    /**
-     * This method is accepts posted JSON containing feedback information. It responds to POST
-     * requests to /feedback.
-     * @return Boolean true indicating success.
-     */
-//    @PostMapping(value = "/", produces = "application/json")
-//    public ResponseEntity addFeedback(@RequestParam(value = "feedbackText") String feedbackText) {
-//        if(loggedUserService.getUsername() == null){
-//            return new ResponseEntity<>(false, null, HttpStatus.FORBIDDEN);
-//        }
-//        Account loggedInUser = accountService.getAccountByEmail(loggedUserService.getUsername());
-//        Feedback feedback = new Feedback(null, loggedInUser.getId(), feedbackText);
-//        feedbackService.insertNewFeedback(feedback);
-//        return new ResponseEntity<>(true, null, HttpStatus.OK);
-//    }
-
-    /**
-     * This method is used to return the JSON for all the feedback entries. It responds to GET
-     * requests to /feedback.
-     * @return ResponseEntity object containing JSON.
-     */
-//    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-//    public ResponseEntity getFeedback() {
-//        List<Feedback> feedback = feedbackService.getAll();
-//
-//        for (Feedback feedbackObject : feedback) {
-//            Account account = accountService.getAccountById(feedbackObject.getUserProfileId());
-//            feedbackObject.setUserEmail(account.getEmail());
-//        }
-//
-//        return new ResponseEntity<>(feedback, null, HttpStatus.OK);
-//    }
-
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity getFeedbackData(){
         FeedbackOverview overview = feedbackService.getFeedbackOverview();
